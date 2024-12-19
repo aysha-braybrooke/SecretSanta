b4be876fe36d400cfd557c65d67e48a78e56f560
aysha-braybrooke <aysha.braybrooke@etu.unilim.fr>
2024-12-19 09:37:51 +0100

    V2.3

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/secret-santa/", // add repository name here
+  base: "/SecretSanta/", // add repository name here
   plugins: [react()],
 });
